bc77b87ba325e6638cfb0cc0b222d96691356235
Cole <griwold.cole@gmail.com>
2013-07-21 03:43:06 -0500

    Client and Common Proxy classes made

---
+++ CommonProxy.java
@@ -0,0 +1,9 @@
+package morePistons.morePistons;
+
+public class CommonProxy {
+	// Client stuff
+    public void registerRenderers() {
+            // Nothing here as the server doesn't render graphics or entities!
+    }
+
+}
