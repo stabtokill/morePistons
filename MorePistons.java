ea46bf1a10015ca1cc2f1492d550067dc5a3c0ba
stabtokill <stabtokill@yahoo.com>
2013-07-21 03:40:23 -0500

    Base class update

--- MorePistons.java
+++ MorePistons.java
@@ -1,5 +1,41 @@
 package morePistons.morePistons;
 
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit; 
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkMod;
+
+@Mod(modid="MorePistons", name="MorePistons", version="0.0.1")
+@NetworkMod(clientSideRequired=true, serverSideRequired=false)
 public class MorePistons {
 
+        // The instance of your mod that Forge uses.
+        @Instance("MorePistons")
+        public static MorePistons instance;
+        
+        // Says where the client and server 'proxy' code is loaded.
+        @SidedProxy(clientSide="morePistons.morePistons.ClientProxy", serverSide="morePistons.morePistons.CommonProxy")
+        public static CommonProxy proxy;
+        
+        @EventHandler
+        public void preInit(FMLPreInitializationEvent event) {
+                // Stub Method
+        }
+        
+        @EventHandler
+        public void load(FMLInitializationEvent event) {
+                proxy.registerRenderers();
+        }
+        
+        @EventHandler
+        public void postInit(FMLPostInitializationEvent event) {
+                // Stub Method
+        }
 }
